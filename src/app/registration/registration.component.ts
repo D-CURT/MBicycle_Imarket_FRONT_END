7060da24b547eae25b2ad62ceade4cb171e9101c
LeftyDead <LeftyForDead@mail.ru>
2018-12-30 18:09:31 +0300

    Working with Spring Security: login and registration.

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+  }
+
+  register() {
+
+    let tosend = this.model.login + this.model.password;
+
+    const options = {headers: {'Content-Type': 'application/json'}};
+    this.http.post('/registration', this.model, options).subscribe(
+        data => console.info('%%%%' + JSON.stringify(data) + "%%%@")  //FIXME: Return something? React on it somehow?
+    );
+
+    // this.http.post<T>('/registration', JSON.stringify(data), options).subscribe(
+    //   (t: T) => console.info(JSON.stringify(t))
+    // );
   }
 
 }
