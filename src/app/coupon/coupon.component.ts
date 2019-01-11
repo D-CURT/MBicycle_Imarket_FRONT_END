2a2e68355b78f5f6b38302ea38a0f97355de50f3
cawendish <Kjvjyjcjd26>
2019-01-11 16:58:43 +0300

    Begin combobox

--- src/app/coupon/coupon.component.ts
+++ src/app/coupon/coupon.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms'
+//import { ReactiveFormsModule } from '@angular/forms';
+import {CurrentRoleService} from '../services/current-role.service';
+
 
 @Component({
   selector: 'app-coupon',
@@ -10,26 +12,52 @@ import { ReactiveFormsModule } from '@angular/forms'
   styleUrls: ['./coupon.component.css']
 })
 export class CouponComponent implements OnInit {
+  model: any = {};
   coupons: any;
-  output: string[];
+  removableCoupons: string[];
+  customs: any;
+
  constructor(
    private http: HttpClient,
-   private router: Router
+   private router: Router,
+   public roleService: CurrentRoleService
   ) { }
   
 ngOnInit() {
-    this.output = [];
+    this.removableCoupons = [];
     this.http.get('/coupons/getAll').subscribe(data => {
       this.coupons = data;
     });
+    this.http.get('/profiles/customers').subscribe(data => {
+      this.customs = data;
+    });
   }
+
+
+  onSubmitAddCupon(){
+
+    console.log(this.model.description);
+    console.log(this.model.sum);
+
+    let couponDTO = {
+      description:this.model.description,
+      sum:this.model.sum,
+    }
+
+    this.http.post('/coupons/add', couponDTO).subscribe(data => {
+      console.log(couponDTO);
+      this.router.navigateByUrl('/index', {skipLocationChange: true}).then(() => this.router.navigate(["/coupon"]));
+    });
+  }
+
+
 
   postDelete() {
     console.log('postdel');
-    console.log(this.output);
-    this.http.post('/coupons/deleteAll', this.output).subscribe(data => {
+    console.log(this.removableCoupons);
+    this.http.post('/coupons/deleteAll', this.removableCoupons).subscribe(data => {
       console.log(data);
-      this.router.navigateByUrl('/index', {skipLocationChange: true}).then(() => this.router.navigate(["/coupons"]));
+      this.router.navigateByUrl('/index', {skipLocationChange: true}).then(() => this.router.navigate(["/coupon"]));
     });
 
     console.log('end post del');
@@ -40,10 +68,10 @@ ngOnInit() {
     console.log('event!');
     if (event.checked) {
       console.log('push!');
-      this.output.push(event.source.name);
+      this.removableCoupons.push(event.source.name);
     } else {
       console.log('else!');
-      this.output.splice(this.output.indexOf(event.source.name), 1);
+      this.removableCoupons.splice(this.removableCoupons.indexOf(event.source.name), 1);
     }
   }
 
